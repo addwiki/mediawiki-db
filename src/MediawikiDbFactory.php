e9cb31365aa2abd7af72b6aa103326070e146db3
addshore <addshorewiki@gmail.com>
2015-08-29 18:28:27 +0200

    Implement MediawikiDbFactory

--- src/MediawikiDbFactory.php
+++ src/MediawikiDbFactory.php
@@ -2,8 +2,29 @@
 
 namespace Mediawiki\Db;
 
+use Mediawiki\Db\Service\PageFileExtensionListGetter;
+use Mediawiki\Db\Service\ProbablyDeletedTitleListGetter;
+use Mediawiki\Db\Service\RedirectListGetter;
+use PDO;
+
 class MediawikiDbFactory {
 
-	//TODO implement me....
+	private $db;
+
+	public function __construct( PDO $db ) {
+		$this->db = $db;
+	}
+
+	public function newPageFileExtensionListGetter( $fileExtension ) {
+		return new PageFileExtensionListGetter( $this->db, $fileExtension );
+	}
+
+	public function newProbablyDeletedTitleListGetter() {
+		return new ProbablyDeletedTitleListGetter( $this->db );
+	}
+
+	public function newRedirectListGetter() {
+		return new RedirectListGetter( $this->db );
+	}
 
 }
